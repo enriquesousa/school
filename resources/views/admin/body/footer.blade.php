23680eda0d362be7de822a84f4eff23b006d0ac4
enriquesousa <enrique.sousa@gmail.com>
2022-02-09 21:37:13 -0800

    128. Dashboard Page Segmentation

---
+++ resources/views/admin/body/footer.blade.php
@@ -0,0 +1,14 @@
+{{-- Main Footer --}}
+<footer class="main-footer">
+    <div class="pull-right d-none d-sm-inline-block">
+        <ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
+            <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0)">FAQ</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">www.admin-escuela.com</a>
+            </li>
+        </ul>
+    </div>
+    &copy; 2022 <a href="#">Enrique Sousa</a>. All Rights Reserved.
+</footer>
