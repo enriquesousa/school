a95a30e9af2a25046a2d02dbe32b3be745ef042b
Enrique <enrique.sousa@gmail.com>
2023-06-28 10:41:15 -0700

    243. Dashboard Page Segmentation

--- bitacora.md
+++ bitacora.md
@@ -7,7 +7,9 @@ Estoy en Laravel 10
 
 # S21 - School Management System 
 ## 242. Customize Admin Dashboard
+Listo!
 ## 243. Dashboard Page Segmentation
+
 ## 244. User Logout Option
 ## 245. Customize Login Form
 ## 246. Refresh Admin Template
---
+++ resources/views/admin/body/footer.blade.php
@@ -0,0 +1,18 @@
+<footer class="main-footer">
+
+    <div class="pull-right d-none d-sm-inline-block">
+        <ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
+            <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0)">PHC (preguntas hechas con frecuencia)</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Adquirir ahora</a>
+            </li>
+        </ul>
+    </div>
+
+    &copy; 2023 <a href="#">Administración de Escuelas</a>. Todos los derechos reservados.
+
+    <p class="text-muted">Hecho con <i class="mb-1 text-primary ms-1 icon-sm" data-feather="heart"></i></p>
+
+</footer>
